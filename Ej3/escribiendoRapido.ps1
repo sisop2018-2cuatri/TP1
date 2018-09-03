d900dad50de3e8bd787ce3e99721c5da6763ad50
mdilorenzo <maximiliano.rdl@gmail.com>
2018-09-03 15:43:11 -0300

    Agregados los headers, validacion en caso de que la cantidad de palabras especificada sea mayor a la del archivo. Falta prueba en 4.0

--- Ej3/escribiendoRapido.ps1
+++ Ej3/escribiendoRapido.ps1
@@ -1,26 +1,75 @@
 <#
-Ejercicio 3:  
- 
-Programar un juego en Powershell que se llame “Escribiendo rápido” (o un nombre más creativo).
- El objetivo del mismo es hacer el mejor tiempo escribiendo una serie de palabras que se irán mostrando por pantalla.  
- 
-Los requerimientos son los siguientes:
-1. Se ejecuta el script. Se muestra por pantalla una palabra. Se empieza un cronómetro. 
-2. El jugador escribe la palabra y presiona Enter. 
-3. Se detiene el cronómetro. Se muestra otra palabra que el usuario escribirá y así sucesivamente hasta que se cumpla la cantidad de palabras ingresada por parámetro. 
-4. Se muestran las estadísticas del juego: tiempo por cada palabra, tiempo total, tiempo promedio y teclas por segundo (sin contar los Enter). 
- 
-El script debe recibir los siguientes parámetros: 
-• Obligatorio. La cantidad de palabras a escribir por el jugador (de 2 a 10). 
-• Opcional. La ruta de un archivo que contenga las palabras a mostrar. Si no se ingresa este parámetro, 
-    el juego debe buscar un archivo llamado “palabras.txt” ubicado en el mismo directorio que el script. 
-• Opcional. La ruta de un archivo que contendrá un listado de los mejores tiempos. Si hay tiempos registrados, 
-    luego de mostrar las estadísticas del juego se deben mostrar los mejores 3 tiempos (debe incluir el último tiempo si corresponde). 
-• Obligatorio: El orden en el que serán mostradas las palabras. Pudiendo ser “Aleatoria”, “Ascendente” o “Descendente”. El parámetro debe ser de tipo switch 
+.Synopsis
+    Juego de escritura de palabras
+.DESCRIPTION
+    El juego consiste en escribir la palabra mostrada en pantalla lo mas rapido posible. El tiempo que se tarde por cada palabra va a decidir el score final (tiempo promedio).
+.EXAMPLE
+    .\escribiendoRapido.ps1 3 palabras.txt score.txt -Aleatoria
+        Bienvenido a EscribiendoRapido!
 
 
-.\escribiendoRapido.ps1 3 '' '' -Descendente
-.\escribiendoRapido.ps1 3 '.\palabras.txt' -Descendente
+        Este juego toma el tiempo que tardas en escribir las palabras mostradas en pantalla.
+
+        Presiona Enter para comenzar: 
+
+        ------
+        START!
+        ------
+
+        cartucho: cartucho
+        letrina: letrina
+        sarampion: sarampion
+
+        ----------
+        FINISHED!
+        ----------
+
+        Tiempo total: 00:00:05.8187271
+        Palabra 'cartucho' / Tiempo: 00:00:02.0732400
+        Palabra 'letrina' / Tiempo: 00:00:01.4023230
+        Palabra 'sarampion' / Tiempo: 00:00:02.3431641
+        Tiempo Promedio por palabra: 00:00:01.9400000
+        Teclas por segundo: 4.12461343993947
+        Ingrese su nombre: JIM
+
+        Name                           Value                                                                                                                                                 
+        ----                           -----                                                                                                                                                 
+        TIM                            00:00:01.4330000                                                                                                                                      
+        maxi                           00:00:01.5600000                                                                                                                                      
+        max                            00:00:01.8870000
+.EXAMPLE
+    .\escribiendoRapido.ps1 2 palabras.txt -Descendente
+        Bienvenido a EscribiendoRapido!
+
+
+        Este juego toma el tiempo que tardas en escribir las palabras mostradas en pantalla.
+
+        Presiona Enter para comenzar: 
+
+        ------
+        START!
+        ------
+
+        letrina: letrina
+        cartucho: cartucho
+
+        ----------
+        FINISHED!
+        ----------
+
+        Tiempo total: 00:00:02.7876103
+        Palabra 'letrina' / Tiempo: 00:00:01.4042077
+        Palabra 'cartucho' / Tiempo: 00:00:01.3834026
+        Tiempo Promedio por palabra: 00:00:01.3940000
+        Teclas por segundo: 5.38095299762668
+    
+.NOTES
+    Nombre del Script: escribiendoRapido.ps1
+    Trabajo Practico: 1
+    Numero de Ejercicio: 3
+
+.Integrantes:
+    38166442 Maximiliano Di Lorenzo
 
 #>
 [CmdletBinding()]
@@ -42,23 +91,29 @@ param (
     [Switch]$Descendente
 
 )
-##$PSCmdlet.ParameterSetName
-##Write-Host($cantidad);
-#| Sort-Object {Get-Random}
 
 Write-Host("`nBienvenido a EscribiendoRapido!`n`n");
 Write-Host("Este juego toma el tiempo que tardas en escribir las palabras mostradas en pantalla.`n");
-Read-Host "Presiona Enter para comenzar" | Out-Null;
-Write-Host("`n------`nSTART!`n------`n");
+
+if((Get-Content $rutaPalabras).Count -lt $cantidad){
+    Write-Error -Message "CANTIDAD DE PALABRAS MAYOR A LAS EXISTENTES EN EL ARCHIVO." -ErrorAction Stop
+}else{
 $palabras = Get-Content -Path $rutaPalabras | select -First $cantidad;
+}
+
 switch($PSCmdlet.ParameterSetName){
     'Aleatoria' {$palabras = $palabras | Sort-Object {Get-Random};}
     'Descendente' {$palabras = $palabras | Sort-Object -Descending;}
     'Ascendente' {$palabras = $palabras | Sort-Object;}
 }
+
 $stopwatch =  New-Object System.Diagnostics.Stopwatch;
 $tiempoTranscurrido = $stopwatch.Elapsed;
 $tiempoPalabra = @{};
+
+Read-Host "Presiona Enter para comenzar" | Out-Null;
+
+Write-Host("`n------`nSTART!`n------`n");
 
 foreach($palabra in $palabras){
     $stopwatch.Start();
@@ -71,11 +126,14 @@ foreach($palabra in $palabras){
     $tiempoPalabra[$palabra] = ($stopwatch.Elapsed - $tiempoTranscurrido);
     $tiempoTranscurrido = $stopwatch.Elapsed;
 }
+
 Write-Host("`n----------`nFINISHED!`n----------`n");
 Write-Host("Tiempo total: " + $stopwatch.Elapsed);
+
 foreach($palabra in $palabras){
     Write-Host("Palabra '$palabra' / Tiempo: " + $tiempoPalabra[$palabra]);
 }
+
 $tiempoPromedio =  [TimeSpan]::FromMilliseconds($stopwatch.Elapsed.TotalMilliseconds/ $cantidad); 
 
 Write-Host("Tiempo Promedio por palabra: $tiempoPromedio");
@@ -90,7 +148,8 @@ if($rutaScore){
     
     $nombre = Read-Host -prompt "Ingrese su nombre";
     $score[$nombre]=$tiempoPromedio;
-    write-host($score.GetEnumerator() | sort value);
+
     $score.GetEnumerator() | sort value | select -First 3;
+    
     Add-Content $rutaScore "$nombre/$tiempoPromedio";
 }
