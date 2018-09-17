e97187793180f1b2875559edb22461af1fd01563
mdilorenzo <maximiliano.rdl@gmail.com>
2018-09-17 01:11:18 -0300

    Agregados comentarios al codigo, paso por paso

--- Ej3/escribiendoRapido.ps1
+++ Ej3/escribiendoRapido.ps1
@@ -69,11 +69,11 @@
     Numero de Ejercicio: 3
 
 .Integrantes:
-    	.Avila, Leandro - 35.537.983
-		.Di Lorenzo, Maximiliano - 38.166.442
-		.Lorenz, Lautaro - 37.661.245
-		.Mercado, Maximiliano - 37.250.369
-		.Sequeira, Eliana - 39.061.003
+    	Avila, Leandro - 35.537.983
+		Di Lorenzo, Maximiliano - 38.166.442
+		Lorenz, Lautaro - 37.661.245
+		Mercado, Maximiliano - 37.250.369
+		Sequeira, Eliana - 39.061.003
 
 #>
 [CmdletBinding()]
@@ -99,64 +99,70 @@ param (
 Write-Host("`nBienvenido a EscribiendoRapido!`n`n");
 Write-Host("Este juego toma el tiempo que tardas en escribir las palabras mostradas en pantalla.`n");
 
-
+#Evaluo si la cantidad de palabras en el archivo es menor a las pedidas por parametro
 if((Get-Content $rutaPalabras).Count -lt $cantidad){
     Write-Error -Message "CANTIDAD DE PALABRAS MAYOR A LAS EXISTENTES EN EL ARCHIVO." -ErrorAction Stop
 }else{
-$palabras = Get-Content -Path $rutaPalabras | Sort-Object {Get-Random}| select -First $cantidad; #Agarro palabras mescladas del archivo y selecciono las primeras $cantidad. + variedad
+$palabras = Get-Content -Path $rutaPalabras | Sort-Object {Get-Random}| select -First $cantidad; #Agarro palabras mescladas del archivo y selecciono las primeras $cantidad. Da mas variedad de palabras
 }
 
+#Según lo que hayan elegido, determino como ordenar las palabras
 switch($PSCmdlet.ParameterSetName){
     'Aleatoria' {$palabras = $palabras | Sort-Object {Get-Random};}
     'Descendente' {$palabras = $palabras | Sort-Object -Descending;}
     'Ascendente' {$palabras = $palabras | Sort-Object;}
 }
 
+#Instancio la clase stopwatch(Cronometro) para poder realizar las mediciones de los tiempos
 $stopwatch =  New-Object System.Diagnostics.Stopwatch;
 $tiempoTranscurrido = $stopwatch.Elapsed;
-$tiempoPalabra = @{};
+$tiempoPalabra = @{}; #Hashtable donde voy a guardar los tiempos de cada palabra
 
 Read-Host "Presiona Enter para comenzar" | Out-Null;
 
+#Inicio del juego
 Write-Host("`n------`nSTART!`n------`n");
 
+#Recorro todas las palabras de la lista, tomando el tiempo que tardan en escribirlas
 foreach($palabra in $palabras){
     $stopwatch.Start();
     do{
         $palabraEscrita = Read-Host -prompt $palabra;
-        $teclas+= $palabraEscrita.Length;
-    }while($palabraEscrita -notlike $palabra);
+        $teclas+= $palabraEscrita.Length; #La cantidad de letras es la longitud de la palabra
+    }while($palabraEscrita -notlike $palabra); #Si la palabra esta mal escrita, la pido devuelta hasta que le salga bien.
     
-    $stopwatch.Stop();
-    $tiempoPalabra[$palabra] = ($stopwatch.Elapsed - $tiempoTranscurrido);
-    $tiempoTranscurrido = $stopwatch.Elapsed;
+    $stopwatch.Stop(); #Paro el cronometro cuando la palabra se escribe correctamente
+    $tiempoPalabra[$palabra] = ($stopwatch.Elapsed - $tiempoTranscurrido); #Como el cronometro no me deja hacer laps, tengo que guardar el tiempo transcurrido hasta el anterior lap y restarle el Elapsed para calcular delta tiempo (el tiempo de la palabra)
+    $tiempoTranscurrido = $stopwatch.Elapsed; 
 }
 
 Write-Host("`n----------`nFINISHED!`n----------`n");
 Write-Host("Tiempo total: " + $stopwatch.Elapsed);
 
+#Muestro las palabras y sus tiempos
 foreach($palabra in $palabras){
     Write-Host("Palabra '$palabra' / Tiempo: " + $tiempoPalabra[$palabra]);
 }
 
-$tiempoPromedio =  [TimeSpan]::FromMilliseconds($stopwatch.Elapsed.TotalMilliseconds/ $cantidad); 
+$tiempoPromedio =  [TimeSpan]::FromMilliseconds($stopwatch.Elapsed.TotalMilliseconds/ $cantidad); #La unica forma que se me ocurrio de calcular el average time por palabra en juego, usando TimeSpan de .Net para poder crear un TimeSpan con milliseconds, cosa que el de PS no me deja
 
 Write-Host("Tiempo Promedio por palabra: $tiempoPromedio");
 Write-Host("Teclas por segundo: "+ $teclas / $stopwatch.Elapsed.TotalSeconds);
 
+#Si hay algun archivo de scores, guardo el score y muestro el top 3
 if($rutaScore){
-    [Hashtable]$score = @{};
+    [Hashtable]$score = @{}; #Hashtable de scores donde voy a trabajar
     Get-Content -Path $rutaScore | ForEach-Object{
         $objeto = $_.ToString().Split("/").trim();
-        $score[$objeto[0]]=$objeto[1];
+        $score[$objeto[0]]=$objeto[1]; #Guardo en el hashtable los scores del archivo
     }
     do{
     $nombre = Read-Host -prompt "Ingrese su nombre con letras de la a-z y numeros 0-9";
-    }while($nombre -notmatch "^[a-z0-9]*$");
+    }while($nombre -notmatch "^[a-z0-9]*$"); #Para que no ingresen caracteres foraneos que revienten el split de arriba
 
-    $score[$nombre]=$tiempoPromedio;
+    $score[$nombre]=$tiempoPromedio; #Agrego al hastable el score del jugador actual
 
-    $score.GetEnumerator() | sort value | select -First 3;
+    $score.GetEnumerator() | sort value | select -First 3; #Del hashtable levanto los mejores 3
     
-    Add-Content $rutaScore "$nombre/$tiempoPromedio";
+    Add-Content $rutaScore "$nombre/$tiempoPromedio"; #Guardo el score del jugador actual al final del archivo
 }
